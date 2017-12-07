63e613e38a762695809044dc77a2d25a4efe4a64
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    lavender: Build dummy android.hidl.base@1.0
     * Required by Oreo-MR0 blobs
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- device.mk
+++ device.mk
@@ -231,10 +231,7 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.base@1.0
 
 # IFAA manager
 PRODUCT_PACKAGES += \
