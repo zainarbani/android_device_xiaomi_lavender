c782dde57a64461fa01b49599e793428ad191637
AngeloGioacchino Del Regno <kholk11@gmail.com>
2019-07-24 18:24:26 +0200

    lavender: Add 32-bits toolchain prefix for VDSO32
    The kernel recently got VDSO32 support and it now requires us
    to compile kernel code targeting ARM/AArch32 (the VDSO32 compat
    code does actually target 32-bits...) to get the feature to work.
    
    Define the 32-bits toolchain to use to compile the VDSO32 in kernel.
    
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_32BITS_PREFIX := arm-linux-androideabi-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
 TARGET_KERNEL_CONFIG := lavender-perf_defconfig
 TARGET_KERNEL_CLANG_VERSION := $(shell grep -v based prebuilts/clang/host/$(HOST_OS)-x86/*/AndroidVersion.txt | sort | tail -n 1 | cut -d : -f 2)
